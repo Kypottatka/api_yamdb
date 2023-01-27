98a46ae1806f4b70ca92aac716a9de7e2b679f42
Andrey Ten <andrey.ten.evg@gmail.com>
2023-01-27 11:41:24 +0600

    IsModerator permission added

--- api_yamdb/users/permissions.py
+++ api_yamdb/users/permissions.py
@@ -1,11 +1,18 @@
 from rest_framework import permissions
 from rest_framework.response import Response
 
-from .models import ADMIN
+from .models import ADMIN, MODERATOR
 
 
 class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.user.is_authenticated:
             return request.user.is_staff or request.user.role == ADMIN
+        return Response(status=403)
+
+
+class IsModerator(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method == 'DELETE' and request.user.is_authenticated:
+            return request.user.role == MODERATOR
         return Response(status=403)
